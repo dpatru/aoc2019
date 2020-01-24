40e43a354e40cf10b9dde16f4cdcd09b95e1b479
Daniel Patru <dpatru@gmail.com>
2020-01-24 18:31:43 -0500

    still working on 25

--- 25.hs
+++ 25.hs
@@ -5,7 +5,7 @@ import Debug.Trace
 
 import System.Environment (getArgs)
 import System.IO (readFile)
-import Data.Map.Strict (Map, (!), insert, elems, fromList, toList, findWithDefault, size, empty, member, findMin, findMax, singleton, filter)
+import Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as M
 import Data.Set (Set)
 import qualified Data.Set as S
@@ -14,10 +14,12 @@ import Data.List (find, intercalate, intersperse, permutations, inits, tails, is
 import Data.List.Split (splitOn)
 -- import Data.Complex (Complex((:+)), realPart, imagPart) -- define my own complex
 
+import Text.Regex.PCRE
+
 import UI.NCurses
 import Data.Char (chr, ord)
 -- import Data.Complex (Complex((:+)))
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, isNothing)
 type Instructions = Map Integer Integer
 
 data ComputerState = Ready | Running | Blocked | Done
@@ -37,7 +39,7 @@ fromAscii s = map (fromIntegral . ord) s
 instance Show Computer where
   show c = "Computer < \""++toAscii (input c)++"\" > \""++toAscii (output c)++"\"\n"
 
-computer0 = Computer { state = Ready, memory = empty, iptr = 0, base = 0, input = [], output = [] }
+computer0 = Computer { state = Ready, memory = M.empty, iptr = 0, base = 0, input = [], output = [] }
 
 run :: Computer -> Computer -- (Integer, Integer) -> Instructions -> [Integer] -> [Integer]
 run c
@@ -52,13 +54,13 @@ run c
     --traceShow (iptr c, base c, memory c) $ 
     case instr `mod` 100 of
       1 -> -- add
-        run $ c {iptr = i+4, memory = insert (addr 3) (arg 1 + arg 2) instructions}
+        run $ c {iptr = i+4, memory = M.insert (addr 3) (arg 1 + arg 2) instructions}
       2 -> -- multiply
-        run $ c {iptr = i+4, memory = insert (addr 3) (arg 1 * arg 2) instructions}
+        run $ c {iptr = i+4, memory = M.insert (addr 3) (arg 1 * arg 2) instructions}
       3 -> -- read input
         if (null $ input c)
         then c {state = Blocked}
-        else run $ c {iptr = i+2, memory = insert (addr 1) (head $ input c) instructions, input = tail $ input c}
+        else run $ c {iptr = i+2, memory = M.insert (addr 1) (head $ input c) instructions, input = tail $ input c}
       4 -> -- output
         --traceShow (arg 1) $ 
         run $ c {iptr = i+2, output = output c ++ [arg 1]}
@@ -67,9 +69,9 @@ run c
       6 -> -- jump-if-false
         run $ c {iptr = if arg 1 == 0 then arg 2 else i+3}
       7 -> -- less than
-        run $ c {iptr = i+4, memory = insert (addr 3) (if arg 1 < arg 2 then 1 else 0) instructions}
+        run $ c {iptr = i+4, memory = M.insert (addr 3) (if arg 1 < arg 2 then 1 else 0) instructions}
       8 -> -- equals
-        run $ c {iptr = i+4, memory = insert (addr 3) (if arg 1 == arg 2 then 1 else 0) instructions}
+        run $ c {iptr = i+4, memory = M.insert (addr 3) (if arg 1 == arg 2 then 1 else 0) instructions}
       9 -> -- set relative base
         run $ c {iptr = i+2, base = base c + arg 1}
       99 -> -- halt
@@ -78,7 +80,7 @@ run c
   where instructions = memory c
         i = iptr c
         instr = instructions!i
-        ii x = findWithDefault 0 x instructions
+        ii x = M.findWithDefault 0 x instructions
         arg :: Integer -> Integer
         arg n = case (instr `mod` (100*10^n)) `div` (10*10^n) of
           0 -> ii $ ii $ i+n
@@ -92,32 +94,73 @@ run c
             ii (i+n) + base c
           _ -> error $ "address in unknown mode"
 
-interactc :: Computer -> IO()
-interactc c = do
-  let out = toAscii $ output c
-  putStrLn out
-  if "Command?" /= last (words out)
-    then return ()
-    else do
-      s <- getLine
-      interactc $ run $ c{output=[], input=fromAscii s ++ [10]}
-  
--- interactc :: Computer -> String -> String
--- interactc c s = unlines $ f c $ lines s
---   where
---     f c [] = []
---     f c (l:ls) = (toAscii $ output c') : f (c'{output = []}) ls
---       where c' = run $ c{input = fromAscii $ unlines [l]} 
+type Direction = String
 
+-- data Direction = N | S | E | W
+-- instance Show Direction where
+--   show N = "north"
+--   show S = "south"
+--   show E = "east"
+--   show W = "west"
+-- instance Read Direction where
+--   readsPrec _ ('n':'o':'r':'t':'h':theRest) = [(N, theRest)]
+--   readsPrec _ ('s':'o':'u':'t':'h':theRest) = [(S, theRest)]
+--   readsPrec _ ('e':'a':'s':'t':theRest) = [(E, theRest)]
+--   readsPrec _ ('w':'e':'s':'t':theRest) = [(W, theRest)]
+  
+type Graph = Map String (Map Direction (Maybe String))
+
+back "north" = "south"
+back "south" = "north"
+back "east" = "west"
+back "west" = "east"
+back d = error $ "unknown door type " ++ d
+
+stripRoomName = reverse . drop 3 . reverse . drop 3 -- strip first 3 and last 3 characters 
+interactc :: Graph -> [(Direction, String)] -> Computer -> IO()
+interactc g explorePath c = do
+  let out = toAscii $ output c
+      linesOut = lines out
+      room = map stripRoomName $ filter (isPrefixOf "==") $ linesOut
+      -- if booted out, then last room listed is current room
+      -- room = map (head . tail) $ (out =~ "== (.+?) ==" :: [[String]]) -- init $ tail $ head linesOut
+      prompt = not $ null $ filter (== "Command?") linesOut
+      stuff = map (drop 2) $ filter (isPrefixOf "- ") linesOut
+      doors = filter (`elem` ["north","south","east","west"]) stuff
+      items = filter (not . (`elem` doors)) stuff
+      newFrontier = not $ (null room || head room `M.member` g)
+      (previousDoor, previousRoom) = head explorePath -- laziness ensures that explorePath is not null
+      g' | newFrontier = M.insert (head room) (M.fromList [(door, Nothing)| door <- doors]) g
+         | null room = g
+         | null explorePath = trace "setting g': no explorePath" g
+         | otherwise = M.adjust (\m -> M.insert (back $ previousDoor) (Just previousRoom) m) (head room) $
+                       M.adjust (\m -> M.insert previousDoor (Just $ head room) m) previousRoom g
+      (move, explorePath')
+        | newFrontier = (head doors, (head doors, head room): explorePath)
+        | null unexplored && null explorePath = error "explored all"
+        | null unexplored || head unexplored == previousRoom = (back previousDoor, tail explorePath)
+        | otherwise = (head unexplored, (head unexplored, head room) : explorePath)
+        where unexplored :: [Direction]
+              unexplored = M.keys $ M.filter isNothing $ g!(head room)
+  putStrLn out
+  putStrLn $ "room: "++ show room ++"\nprompt: "++ show prompt++"\ndoors: "++ show doors++"\nitems: "++show items
+  if prompt then do
+    -- s <- getLine
+    -- interactc g $ run $ c{output=[], input=fromAscii s ++ [10]}
+    putStrLn move
+    putStrLn $  "(explorePath = "++show explorePath++")"
+    interactc g' explorePath' $ run $ c{output=[], input=fromAscii move ++ [10]}
+  else return ()
+  
 
 main = do
   print $ toAscii $ fromAscii "This is a test"
   -- [instructionFile] <- getArgs
   instructionStrings <- readFile "25.input.txt" -- instructionFile
-  let instructions = fromList . zip [0 ..] $ map read $ splitOn "," instructionStrings
+  let instructions = M.fromList . zip [0 ..] $ map read $ splitOn "," instructionStrings
 
   putStrLn "Part 1"
-  interactc $ run $ computer0{memory = instructions}
+  interactc M.empty [] $ run $ computer0{memory = instructions}
   -- let c = run $ computer0{memory = instructions}
   -- let view = output c
   -- let viewStr = map (chr . fromIntegral) view
